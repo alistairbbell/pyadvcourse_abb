3cd10ff8f93b6f16c7bcdcca09880406ab5de846
alistairbbell <alistairbbell@gmail.com>
2023-06-09 15:45:20 +0200

    "Added function to firstfunct. Changed VCS"

--- src/pyadvcourse_abb/first_function.py
+++ src/pyadvcourse_abb/first_function.py
@@ -1,12 +1,15 @@
 import numpy as np
+import scipy as sci
 
 def  addition_subtraction(a,b,c):
     return a+b-c
 
-
 def division_multiply(a,b,c):
     return(a*b)/c
-
-
+#%%
 def newfunction(w):
     return 100*w
+
+#%%
+newfunction(5)
+x = 3
